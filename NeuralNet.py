4c17e9e9f19be45cbe35bea9e8bb2da111ad6913
NickRoss <nross3@nd.edu>
2022-12-08 11:01:54 -0500

    updated code for NeuralNet and RemoveFiles

--- NeuralNet.py
+++ NeuralNet.py
@@ -4,12 +4,12 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_curve
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Dense,  Conv2D, Flatten
-
-path = r'C:\Users\Nick\Downloads\PatientPlots\TestNeuralNet\**\*.npy'
+# Test folder holds a "Lesion" and "NoLesion" folder that contain tumor and no tumor data, respectively
+path = r'C:\Users\Nick\Downloads\TumorStatus\**\*.npy'
 # path = r'G:\My Drive\Notre Dame\Classes\ComputerVision\CVproject-BreastCancer\SandhyaData\CVProject\PatientPlots\**\*.txt'
 # print(path)
 files = glob.glob(path, recursive=False)
@@ -21,10 +21,12 @@ Y = []
 for i in files:
     a = np.load(i)
     X.append(a)
-    if fnmatch.fnmatch(i,"*Lesions*"):
-        b = 1
-    else:
+    if fnmatch.fnmatch(i,"*NoTumor*"):
+        # print(i)
         b = 0
+    else:
+        b = 1
+
     Y.append(b)
 #change vars from list to array
 X = np.array(X)
@@ -36,6 +38,9 @@ Y = to_categorical(Y)
 # print(X)
 
 ## set training and testing data ##
+# Set 90% to training data and 10% to testing data. 10% of original data will also be split into validation data
+# final split is 80% training, 10% validation, and 10% testing
+# set random_state to ensure data splits are the same each test
 x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size = 0.90, random_state= 42)
 print(x_train.shape)
 print(x_test.shape)
@@ -44,26 +49,27 @@ print(y_test.shape)
 
 y_train = y_train.astype("float32")
 y_test = y_test.astype("float32")
+
 #separate some files for validation dataset
-x_valid = x_train[-15933:]
-y_valid = y_train[-15933:]
-x_train = x_train[:-15933]
-y_train = y_train[:-15933]
+x_valid = x_train[-15932:]
+y_valid = y_train[-15932:]
+x_train = x_train[:-15932]
+y_train = y_train[:-15932]
 
 #define input shape
 input_shape = (6,6,1)
 # define the model
 model = Sequential()
-model.add(Conv2D(filters=3, kernel_size = (3,3), input_shape = input_shape, activation = 'relu'))
-model.add(Conv2D(filters=3, kernel_size = (3,3), input_shape = input_shape, activation = 'relu'))
+model.add(Conv2D(filters=6, kernel_size = (3,3), input_shape = input_shape, activation = 'relu'))
+model.add(Conv2D(filters=36, kernel_size = (3,3), input_shape = input_shape, activation = 'relu'))
 model.add(Flatten())
 model.add(Dense(1000, activation='relu'))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(2, activation='softmax'))
-epochs = 10
+epochs = 25
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 checkpoint = tf.keras.callbacks.ModelCheckpoint("brstTumor.h5", monitor="val_accuracy",verbose=1, save_best_only=True, save_weights_only=False, mode='auto', save_freq='epoch')
-model.fit(x_train, y_train, epochs=epochs, verbose=1,  validation_data = (x_valid, y_valid),callbacks=[checkpoint])
+history = model.fit(x_train, y_train, epochs=epochs, verbose=1,  validation_data = (x_valid, y_valid),callbacks=[checkpoint])
 
 ## used for testing with test dataset ##
 print('-TESTING-----------------------------')
@@ -72,7 +78,7 @@ score = model.evaluate(x_test, y_test)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 # # evaluate predictions and compare predictions to true results
-print("Generate predictions for 100 samples")
+# print("Generate predictions for 100 samples")
 predictions = model.predict(x_test)
 
 y_test_arg=np.argmax(y_test,axis=1)
@@ -82,6 +88,32 @@ print('Confusion Matrix')
 print(cm)
 # print(predictions)
 # print(y_test[:100])
-disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-disp.plot
+
+# I had issues when running the whole code with this included
+# running this separately actually worked and was able to see the confusion
+# matrix plotted in a separate window
+plt.figure(1)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+
+plt.figure(2)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+
+plt.figure(3)
+## used to generate the ROC curve for the test data
+fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test_arg, Y_pred)
+plt.plot(fpr_keras,tpr_keras)
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+
+ConfusionMatrixDisplay.from_predictions(y_test_arg, Y_pred)
 plt.show()
--- RemoveFiles.py
+++ RemoveFiles.py
@@ -3,16 +3,57 @@ import os
 import glob
 import fnmatch
 import re
+import shutil
 # # #
-# # # path = r'G:/My Drive/Notre Dame/Classes/ComputerVision/CVproject-BreastCancer/SandhyaData/CVProject/PatientPlots/**/*.png'
-# path = r'C:\Users\Nick\Downloads\PatientPlots\**\*.npy'
-# # # print(path)
-# fileList = glob.glob(path, recursive=True)
-# # print(fileList)
+#################################
 #
-# for file in fileList:
-#     os.remove(file)
+## this part of the code will move files from one location to another, specifically into a subdirectory within a directory
+path = r'C:\Users\Nick\Downloads\PatientPlots\**\**\*.npy'
+# print(path)
+fileList = glob.glob(path, recursive=False)
+# print(fileList)
+## create list with strings to segment tumor from non-tumor data ##
+tumors = ['DKU01*Right*', 'DKU02*Right*', 'DKU03*Left*', 'DKU05*Right*', 'DKU06*', 'DKU07*Right*', 'DKU08*Left*',
+          'DKU09*Left*', 'DKU10*Right*', 'DKU11*Left*', 'DKU12*Left*', 'DKU13*Right*', 'DKU14*', 'DKU15*Right*',
+          'DKU16*Right*', 'DKU18*Left*', 'DKU21*Left*', 'DKU22*', 'DKU24*Left*', 'DKU25*Left*', 'DKU26*Left*',
+          'DKU27*Left*', 'DKU29*Left*', 'DKU30*Right*', 'DKU31*Left*', 'DKU32*Right*']
+tumor_dir = r'C:\Users\Nick\Downloads\TumorStatus\Tumor\\'
+notumor_dir = r'C:\Users\Nick\Downloads\TumorStatus\NoTumor\\'
+
+for file in fileList:
     # print(file)
+    ## use if you want to see title which breast and chromophore is being plotted
+    # extract only the file name portion of the path
+    # dirName =os.path.dirname(file)
+    file_name = os.path.basename(file)
+    # print(file_name)
+
+    # if file name contains string of interest then rename/move to folder
+    for tum in tumors:
+        if fnmatch.fnmatch(file_name, tum): # use if looking to match file name with string
+            # print(file_name)
+            shutil.copy(file, tumor_dir)
+        else:
+            # print(file_name)
+            shutil.copy(file, notumor_dir)
+
+# path = r'G:/My Drive/Notre Dame/Classes/ComputerVision/CVproject-BreastCancer/SandhyaData/CVProject/PatientPlots/**/*.png'
+path2 = r'C:\Users\Nick\Downloads\TumorStatus\NoTumor\*.npy'
+# # # print(path)
+fileList2 = glob.glob(path2, recursive=True)
+# print(fileList)
+# tumors = ['DKU01*Right*', 'DKU02*Right*', 'DKU03*Left*', 'DKU05*Right*', 'DKU06*', 'DKU07*Right*', 'DKU08*Left*',
+#           'DKU09*Left*', 'DKU10*Right*', 'DKU11*Left*', 'DKU12*Left*', 'DKU13*Right*', 'DKU14*', 'DKU15*Right*',
+#           'DKU16*Right*', 'DKU18*Left*', 'DKU21*Left*', 'DKU22*', 'DKU24*Left*', 'DKU25*Left*', 'DKU26*Left*',
+#           'DKU27*Left*', 'DKU29*Left*', 'DKU30*Right*', 'DKU31*Left*', 'DKU32*Right*']
+for file2 in fileList2:
+    file_name = os.path.basename(file2)
+    for tum in tumors:
+        if fnmatch.fnmatch(file_name, tum): # use if looking to match file name with string  os.remove(file)
+            print(file_name)
+            os.remove(file2)
+
+## extra lines of code for quick coding/helpful items
 # # extract only the file name portion of the path
 #     file_name = os.path.splitext(os.path.basename(file))[0]
 # # extract only the directory portion of the path
@@ -27,27 +68,13 @@ import re
 #     print('\n')
 # #
 
-#################################
-# this part of the code will move files from one location to another, specifically into a subdirectory within a directory
-path = r'C:\Users\Nick\Downloads\PatientPlots\**\Interpolated\*.npy'
-# print(path)
-fileList = glob.glob(path, recursive=False)
-# print(fileList)
-for file in fileList:
-    # print(file)
-    ## use if you want to see title which breast and chromophore is being plotted
-    # extract only the file name portion of the path
-    # dirName =os.path.dirname(file)
-    file_name = os.path.basename(file)
-    # print(file_name)
+###########################################
+## used for renaming files ##
     # extract only the directory portion of the path
     # firstThree = file_name[0:3]
     # print(firstThree)
     # noise = r'\Noise\\'
     # print(dirName+noise+file_name)
-    # if file name contains string of interest then rename/move to folder
-    # if fnmatch.fnmatch(firstThree, '*n'): # use if looking to match file name with string
-    #     # print(file)
     #     # print(dirName+noise+file_name)
     #     newFile = file_name[2:9]+file_name[0:2]+file_name[8:]
     #     newFile = (dirName + '\\' + newFile)
@@ -56,4 +83,3 @@ for file in fileList:
     #     newFile = file_name[3:9] + '-' + file_name[0:2]+file_name[9:]
     #     newFile = (dirName + '\\' + newFile)
     #     os.rename(file, newFile)
-
